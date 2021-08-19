5ea656cceb715044091701a6b52d92bd6358f068
Rafay Siddiqui <rafays.siddiqui@gmail.com>
2021-08-19 00:33:14 +0000

    Query average total duration per cohort

---
+++ 4_queries/9_average_total_duration_per_cohort.sql
@@ -0,0 +1,8 @@
+SELECT AVG(total_duration) AS average_total_duration 
+FROM (
+  SELECT cohorts.name as cohort, sum(completed_at - started_at) AS total_duration
+  FROM assistance_requests
+  JOIN students ON student_id = students.id
+  JOIN cohorts ON cohort_id = cohorts.id
+  GROUP BY cohort
+) total_durations;
