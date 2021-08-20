90b1f7d4ab014c22a5af882208746ac898c40d39
Rafay Siddiqui <rafays.siddiqui@gmail.com>
2021-08-20 03:37:10 +0000

    Parametrize input arguments of students.js and teachers.js

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -9,13 +9,15 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
+const values = [`${args[0]}%`, args[1] || 5]
+
 pool.query(`
 SELECT students.id as student_id, students.name as name, cohorts.name as cohort
 FROM students
 JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name LIKE '${args[0]}%'
-LIMIT ${args[1] || 5};
-`)
+WHERE cohorts.name LIKE $1
+LIMIT $2;
+`, values)
 .then(res => {
   res.rows.forEach(user => {
     console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -9,15 +9,17 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
+const values = [`${args[0] || 'JUL02'}%`]
+
 pool.query(`
 SELECT DISTINCT teachers.name AS teacher, cohorts.name AS cohort
 FROM teachers
 JOIN assistance_requests ON teachers.id = teacher_id
 JOIN students ON student_id = students.id
 JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name LIKE '${args[0]}'
+WHERE cohorts.name LIKE $1
 ORDER BY teachers.name;
-`)
+`, values)
 .then(res => {
   console.log('connected');
   res.rows.forEach(row => {
